9398511a4bbd44574850d1b6e6c8f52f42b3ad31
Max Machon <max@machon.family>
2023-02-10 00:51:43 -0500

    refactor main
    Drawing to the terminal with drawille is far more performant and smooth.
    Generation calculation is now more efficient and concise.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Conway's Game of Life
 
-To start: change input.txt and `cargo run`
+Start by typing `cargo run --release`
--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,74 @@
-use std::thread::sleep;
-use std::time::{Duration, Instant};
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+extern crate drawille;
+extern crate rand;
+
+use drawille::Canvas;
+use rand::Rng;
+use std::{thread, time};
 
 fn main() {
-  let mut grid: [[bool; 50]; 50] = get_input();
+    let width: u32 = 100;
+    let height: u32 = 100;
 
-  let tick_time = Duration::from_millis(100); // 10 TPS
+    let mut canvas = generate_random_canvas(width, height);
 
-  let mut ticks: u64 = 0;
-
-  // main game loop
-  loop {
-    let now = Instant::now();
-
-    draw(&grid);
-    println!("Ticks: {}", ticks);
-    grid = tick(grid);
-    ticks += 1;
-
-    let mut sleep_time = tick_time.checked_sub(now.elapsed());
-    if sleep_time == None {
-      sleep_time = Some(Duration::from_millis(0));
+    let mut generations: u64 = 0;
+    loop {
+        draw_frame(&canvas);
+        println!("Generations: {}", generations);
+        canvas = compute_tick(&canvas, width, height);
+        generations += 1;
+        thread::sleep(time::Duration::from_millis(75));
     }
-    println!(
-      "Frame Time: {:.3}ms",
-      tick_time.checked_sub(sleep_time.unwrap()).unwrap().subsec_nanos() as f64 * 1e-6);
-    sleep(sleep_time.unwrap());
-  }
 }
 
-fn get_input() -> [[bool; 50]; 50] {
-  let path = Path::new("input.txt");
-  let display = path.display();
-
-  // Open read only
-  let mut file = match File::open(&path) {
-    Err(why) => panic!("couldn't open {}: {}", display, why.to_string()),
-    Ok(file) => file,
-  };
-
-  let mut s = String::new();
-  match file.read_to_string(&mut s) {
-    Err(why) => panic!("couldn't read {}: {}", display, why.to_string()),
-    Ok(_) => {},
-  };
-
-  let mut grid: [[bool; 50]; 50] = [[false; 50]; 50];
-  let mut x: usize;
-  let mut y: usize = 0;
-  for i in s.split('\n') {
-    x = 0;
-    for j in i.split("") {
-      if j == "1" {
-        grid[y % 50][x % 50] = true;
-      }
-      x += 1;
-    }
-    y += 1;
-  }
-
-  grid
-}
-
-// % (modulo) is not the same as getting the remainder in Rust
-fn rem(a: i64, b: i64) -> i64 {
-  ((a % b) + b) % b
-}
-
-fn tick(grid: [[bool; 50]; 50]) -> [[bool; 50]; 50] {
-  let mut next: [[bool; 50]; 50] = [[false; 50]; 50];
-  for i in 0..50 {
-    for j in 0..50 {
-      // Check neighbors
-      // TODO: Find a better way to do this
-      let mut neighbors: u8 = 0;
-      let x: i64 = i as i64;
-      let y: i64 = j as i64;
-      let xm: usize = rem((x - 1), 50) as usize;
-      let xp: usize = rem((x + 1), 50) as usize;
-      let ym: usize = rem((y - 1), 50) as usize;
-      let yp: usize = rem((y + 1), 50) as usize;
-
-      if grid[i][ym] { neighbors += 1; }
-      if grid[i][yp] { neighbors += 1; }
-      if grid[xm][j] { neighbors += 1; }
-      if grid[xp][j] { neighbors += 1; }
-      if grid[xm][ym] { neighbors += 1; }
-      if grid[xp][yp] { neighbors += 1; }
-      if grid[xm][yp] { neighbors += 1; }
-      if grid[xp][ym] { neighbors += 1; }
-
-      if grid[i][j] { // Alive
-        if neighbors == 2 || neighbors == 3 {
-          next[i][j] = true;
+fn compute_tick(canvas: &Canvas, width: u32, height: u32) -> Canvas {
+    let mut new_canvas = Canvas::new(width, height);
+    for x in 0..width {
+        for y in 0..height {
+            let neighbors = count_neighbors(&canvas, x, y);
+            if canvas.get(x, y) {
+                if neighbors == 2 || neighbors == 3 {
+                    new_canvas.set(x, y);
+                }
+            } else {
+                if neighbors == 3 {
+                    new_canvas.set(x, y);
+                }
+            }
         }
-      } else { // Dead
-        if neighbors == 3 {
-          next[i][j] = true;
-        }
-      }
     }
-  }
-  next
+    new_canvas
 }
 
-fn draw(grid: &[[bool; 50]; 50]) {
-  // Clear screen
-  print!("{}[2J", 27 as char);
-
-  println!(" -----------------------------------------------------------------------------------------------------");
-  for x in 0..50 {
-    print!("| ");
-    for y in 0..50 {
-      if grid[x][y] {
-        print!("* ");
-      } else {
-        print!("  ");
-      }
+fn count_neighbors(canvas: &Canvas, x: u32, y: u32) -> u32 {
+    let mut count = 0;
+    for i in x - 1..x + 2 {
+        for j in y - 1..y + 2 {
+            if canvas.get(i, j) {
+                count += 1;
+            }
+        }
     }
-    println!("|");
-  }
-  println!(" -----------------------------------------------------------------------------------------------------");
+    if canvas.get(x, y) {
+        count -= 1;
+    }
+    count
+}
+
+fn draw_frame(canvas: &Canvas) {
+    clear_screen();
+    println!("{}", canvas.frame());
+}
+
+fn clear_screen() {
+    print!("{}[2J", 27 as char);
+}
+
+fn generate_random_canvas(width: u32, height: u32) -> Canvas {
+    let mut canvas = Canvas::new(width, height);
+    let mut rng = rand::thread_rng();
+    for _i in 0..width * height / 2 {
+        canvas.set(rng.gen_range(0..width), rng.gen_range(0..height));
+    }
+    canvas
 }
