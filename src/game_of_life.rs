93ca77e22a8e0f8348c2b70e791d53666e127ce8
Pragma8123 <max@machon.family>
2023-02-12 22:34:15 -0500

    chore(release): v1.0.0

---
+++ src/game_of_life.rs
@@ -0,0 +1,76 @@
+extern crate drawille;
+extern crate rand;
+
+use drawille::Canvas;
+use rand::Rng;
+
+pub struct Game {
+  pub canvas: Canvas,
+  pub width: u32,
+  pub height: u32,
+  generations: u64,
+}
+
+impl Game {
+  pub fn new(width: u32, height: u32) -> Self {
+    Self {
+      canvas: Self::generate_random_canvas(width, height),
+      width,
+      height,
+      generations: 0,
+    }
+  }
+
+  pub fn tick(&mut self) {
+    let mut new_canvas = Canvas::new(self.width, self.height);
+
+    for x in 0..self.width {
+      for y in 0..self.height {
+        let neighbors = self.count_neighbors(x, y);
+        if self.canvas.get(x, y) {
+          if neighbors == 2 || neighbors == 3 {
+            new_canvas.set(x, y);
+          }
+        } else if neighbors == 3 {
+          new_canvas.set(x, y);
+        }
+      }
+    }
+
+    self.canvas = new_canvas;
+    self.generations += 1;
+  }
+
+  pub fn draw(&self) -> String {
+    format!("{}[2J{}\nGeneration: {}", 27 as char, self.canvas.frame(), self.generations)
+  }
+
+  fn count_neighbors(&self, x: u32, y: u32) -> u32 {
+    let mut count = 0;
+
+    for i in x.saturating_sub(1)..x.min(self.width - 2) + 2 {
+        for j in y.saturating_sub(1)..y.min(self.width - 2) + 2 {
+            if self.canvas.get(i, j) {
+                count += 1;
+            }
+        }
+    }
+
+    if self.canvas.get(x, y) {
+        count -= 1;
+    }
+
+    count
+  }
+
+  fn generate_random_canvas(width: u32, height: u32) -> Canvas {
+    let mut canvas = Canvas::new(width, height);
+    let mut rng = rand::thread_rng();
+
+    for _i in 0..width * height {
+        canvas.set(rng.gen_range(0..width), rng.gen_range(0..height));
+    }
+
+    canvas
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,32 @@
-extern crate drawille;
-extern crate rand;
+extern crate clap;
 
-use drawille::Canvas;
-use rand::Rng;
+mod game_of_life;
+
+use crate::game_of_life::Game;
 use std::{thread, time};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(about = "A simple implementation of Conway's Game of Life")]
+struct Args {
+    #[arg(short = 'W', long, default_value = "100")]
+    width: u32,
+
+    #[arg(short = 'H', long, default_value = "100")]
+    height: u32,
+}
 
 fn main() {
-    let width: u32 = 100;
-    let height: u32 = 100;
+    let args = Args::parse();
 
-    let mut canvas = generate_random_canvas(width, height);
+    let mut game = Game::new(args.width, args.height);
+    game_loop(&mut game);
+}
 
-    let mut generations: u64 = 0;
+fn game_loop(game: &mut Game) {
     loop {
-        draw_frame(&canvas);
-        println!("Generations: {}", generations);
-        canvas = compute_tick(&canvas, width, height);
-        generations += 1;
-        thread::sleep(time::Duration::from_millis(75));
+        println!("{}", game.draw());
+        game.tick();
+        thread::sleep(time::Duration::from_millis(33));
     }
-}
-
-fn compute_tick(canvas: &Canvas, width: u32, height: u32) -> Canvas {
-    let mut new_canvas = Canvas::new(width, height);
-    for x in 0..width {
-        for y in 0..height {
-            let neighbors = count_neighbors(&canvas, x, y);
-            if canvas.get(x, y) {
-                if neighbors == 2 || neighbors == 3 {
-                    new_canvas.set(x, y);
-                }
-            } else {
-                if neighbors == 3 {
-                    new_canvas.set(x, y);
-                }
-            }
-        }
-    }
-    new_canvas
-}
-
-fn count_neighbors(canvas: &Canvas, x: u32, y: u32) -> u32 {
-    let mut count = 0;
-    for i in x - 1..x + 2 {
-        for j in y - 1..y + 2 {
-            if canvas.get(i, j) {
-                count += 1;
-            }
-        }
-    }
-    if canvas.get(x, y) {
-        count -= 1;
-    }
-    count
-}
-
-fn draw_frame(canvas: &Canvas) {
-    clear_screen();
-    println!("{}", canvas.frame());
-}
-
-fn clear_screen() {
-    print!("{}[2J", 27 as char);
-}
-
-fn generate_random_canvas(width: u32, height: u32) -> Canvas {
-    let mut canvas = Canvas::new(width, height);
-    let mut rng = rand::thread_rng();
-    for _i in 0..width * height / 2 {
-        canvas.set(rng.gen_range(0..width), rng.gen_range(0..height));
-    }
-    canvas
 }
