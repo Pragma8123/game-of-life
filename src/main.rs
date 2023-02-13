d8a058a58d8daddfc141477e8942d7a3097f206a
Pragma8123 <max@machon.family>
2023-02-13 14:14:59 -0500

    feat(cli): :sparkles: add game speed cli option
    This option will allow the user to choose how many generations per second the game runs at.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate clap;
 mod game_of_life;
 
 use crate::game_of_life::Game;
-use std::{thread, time};
+use std::{thread, time::Duration, time::Instant};
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -16,20 +16,25 @@ struct Args {
     /// Height of the game board
     #[arg(short = 'H', long, default_value = "100")]
     height: u32,
+
+    /// Simulation speed in generations per second
+    #[arg(short = 'S', long, default_value = "10", )]
+    speed: u8,
 }
 
 fn main() {
     let args = Args::parse();
 
+    let frame_duration: Duration = Duration::from_secs(1) / args.speed as u32;
     let mut game = Game::new(args.width, args.height);
-    game_loop(&mut game);
-}
-
-fn game_loop(game: &mut Game) {
     loop {
-        println!("{}", game.draw());
+        let start = Instant::now();
         game.tick();
-        thread::sleep(time::Duration::from_millis(33));
+        println!("{}", game.draw());
+        let elapsed = start.elapsed();
+        if elapsed < frame_duration {
+            thread::sleep(frame_duration - elapsed);
+        }
     }
 }
 
